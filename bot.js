6565f320b674b0d61f827a69f2ab660adc6abb30
AutoFTbot <151357338+AutoFTbot@users.noreply.github.com>
2024-09-10 09:45:10 +0700

    Create bot.js
    AutoFtbot

---
+++ bot.js
@@ -0,0 +1,221 @@
+const { Telegraf, Markup } = require('telegraf');
+const axios = require('axios');
+
+// Token bot Telegram
+const token = "#";
+const bot = new Telegraf(token);
+// API Key Cloudflare
+const apiKey = "#"; //https://dash.cloudflare.com/profile/api-tokens
+const domaincf = "#"; 
+const iniemail = "#";
+
+const userContext = {};
+
+const getZoneId = async (domain) => {
+    const baseDomain = domaincf;
+    const response = await axios.get(`https://api.cloudflare.com/client/v4/zones?name=${baseDomain}&status=active`, {
+        headers: {
+            'X-Auth-Email': iniemail,
+            'X-Auth-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.data.result.length === 0) {
+        throw new Error('Zone ID not found for the given domain');
+    }
+
+    return response.data.result[0].id;
+};
+
+const getRecordId = async (zoneId, domain) => {
+    const response = await axios.get(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?name=${domain}`, {
+        headers: {
+            'X-Auth-Email': iniemail,
+            'X-Auth-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data.result[0]?.id;
+};
+
+const getRecordByIp = async (zoneId, ip) => {
+    const response = await axios.get(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?content=${ip}`, {
+        headers: {
+            'X-Auth-Email': iniemail, 
+            'X-Auth-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data.result.length > 0;
+};
+
+const createDnsRecord = async (zoneId, domain, ip) => {
+    const response = await axios.post(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`, {
+        type: 'A',
+        name: domain,
+        content: ip,
+        proxied: false
+    }, {
+        headers: {
+            'X-Auth-Email': iniemail,
+            'X-Auth-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return response.data.result.id;
+};
+
+const updateDnsRecord = async (zoneId, recordId, domain, ip) => {
+    const response = await axios.put(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${recordId}`, {
+        type: 'A',
+        name: domain,
+        content: ip,
+        proxied: false
+    }, {
+        headers: {
+            'X-Auth-Email': iniemail,
+            'X-Auth-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.data.success) {
+        console.log(`DNS record updated: ${domain} -> ${ip}`);
+        return response.data.result.id;
+    } else {
+        console.error('Failed to update DNS record:', response.data.errors);
+        return null;
+    }
+};
+
+const deleteDnsRecord = async (zoneId, recordId) => {
+    const response = await axios.delete(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${recordId}`, {
+        headers: {
+            'X-Auth-Email': iniemail,
+            'X-Auth-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.data.success) {
+        console.log(`DNS record deleted: ${recordId}`);
+        return true;
+    } else {
+        console.error('Failed to delete DNS record:', response.data.errors);
+        return false;
+    }
+};
+
+const createRecord = async (domain, ip) => {
+    try {
+        const zoneId = await getZoneId(domain);
+        const recordExists = await getRecordByIp(zoneId, ip);
+        if (recordExists) {
+            throw new Error('DNS record with the same IP already exists');
+        }
+
+        let recordId = await getRecordId(zoneId, domain);
+
+        if (!recordId) {
+            recordId = await createDnsRecord(zoneId, domain, ip);
+        }
+
+        return await updateDnsRecord(zoneId, recordId, domain, ip);
+    } catch (error) {
+        if (error.response) {
+            console.error('Error creating/updating DNS record:', error.response.data);
+        } else {
+            console.error('Error creating/updating DNS record:', error.message);
+        }
+        return null;
+    }
+};
+
+bot.start((ctx) => {
+    ctx.replyWithPhoto('https://github.com/AutoFTbot/AutoFTbot/raw/main/assets/programmer.gif', {
+        caption: '𝘞𝘦𝘭𝘤𝘰𝘮𝘦! 𝘜𝘴𝘦 𝘵𝘩𝘦 𝘣𝘶𝘵𝘵𝘰𝘯𝘴 𝘣𝘦𝘭𝘰𝘸 𝘵𝘰 𝘢𝘥𝘥 𝘢 𝘯𝘦𝘸 𝘐𝘗 𝘰𝘳 𝘷𝘪𝘦𝘸 𝘋𝘕𝘚 𝘳𝘦𝘤𝘰𝘳𝘥𝘴.',
+        ...Markup.inlineKeyboard([
+            [Markup.button.callback('𝘗𝘖𝘐𝘕𝘛𝘐𝘕𝘎 𝘋𝘕𝘚', 'add_ip')],
+            [Markup.button.callback('𝘓𝘪𝘴𝘵 𝘋𝘕𝘚 𝘙𝘦𝘤𝘰𝘳𝘥𝘴/𝘥𝘦𝘭𝘦𝘵𝘦', 'list_dns_records')]
+        ]).resize()
+    });
+});
+
+bot.action('add_ip', (ctx) => {
+    userContext[ctx.from.id] = 'awaiting_ip'; // Set status user
+    ctx.reply('⚠️ *Please send the IP you want to register.*', { parse_mode: 'Markdown' });
+});
+
+bot.on('text', (ctx) => {
+    const userId = ctx.from.id;
+    const status = userContext[userId];
+
+    if (status === 'awaiting_ip') {
+        const ip = ctx.message.text;
+        const domain = `${Math.random().toString(36).substring(2, 7)}.${domaincf}`;
+        createRecord(domain, ip).then((recordId) => {
+            if (recordId) {
+                ctx.reply(`✅ *Registration Successful*\n*VPS IP:* \`${ip}\`\n*Domain:* \`${domain}\``, { parse_mode: 'Markdown' });
+            } else {
+                ctx.reply('⚠️ *Failed to create DNS record.*', { parse_mode: 'Markdown' });
+            }
+            delete userContext[userId]; 
+        }).catch((error) => {
+            ctx.reply('⚠️ *Error occurred while processing your request.*', { parse_mode: 'Markdown' });
+            delete userContext[userId]; 
+        });
+    }
+});
+
+bot.action('list_dns_records', async (ctx) => {
+    try {
+        const zoneId = await getZoneId(domaincf);
+        const response = await axios.get(`https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`, {
+            headers: {
+                'X-Auth-Email': iniemail,
+                'X-Auth-Key': apiKey,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const records = response.data.result;
+        if (records.length === 0) {
+            ctx.reply('No DNS records found.');
+        } else {
+            const recordList = records.map(record => `Domain: ${record.name}\nIP: ${record.content}`).join('\n');
+            const buttons = [];
+            for (let i = 0; i < records.length; i += 2) {
+                buttons.push([
+                    Markup.button.callback(`${records[i].name}`, `delete_${records[i].id}`),
+                    records[i + 1] ? Markup.button.callback(`${records[i + 1].name}`, `delete_${records[i + 1].id}`) : null
+                ].filter(Boolean));
+            }
+            ctx.reply(`DNS Records List:\n\n${recordList}`, Markup.inlineKeyboard(buttons).resize());
+        }
+    } catch (error) {
+        console.error('Error fetching DNS records:', error.message);
+        ctx.reply('⚠️ *Failed to fetch DNS records.*', { parse_mode: 'Markdown' });
+    }
+});
+
+bot.action(/delete_(.+)/, async (ctx) => {
+    const recordId = ctx.match[1];
+    try {
+        const zoneId = await getZoneId(domaincf);
+        const success = await deleteDnsRecord(zoneId, recordId);
+        if (success) {
+            ctx.reply('✅ *DNS record successfully deleted.*', { parse_mode: 'Markdown' });
+        } else {
+            ctx.reply('⚠️ *Failed to delete DNS record.*', { parse_mode: 'Markdown' });
+        }
+    } catch (error) {
+        console.error('Error deleting DNS record:', error.message);
+        ctx.reply('⚠️ *Failed to delete DNS record.*', { parse_mode: 'Markdown' });
+    }
+});
+console.log('AutoFtbot is running...');
+bot.launch();
